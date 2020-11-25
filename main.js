b5032c24a788b690ae9969fa4d3969ade0f86c49
Yuri Sanguino Rodriguez <=>
2020-11-24 19:28:35 -0500

    operadores

---
+++ main.js
@@ -0,0 +1,91 @@
+/*Estos fragmentos de código son para visualizar mensajes en consola del navegador
+y la ventana de DMO
+console.log("Soy el archivo main.js conectado correctamente");
+alert("Soy el archivo main.js conectado correctamente");*/
+
+/*let nombre_usuario;
+nombre_usuario= prompt("Ingrese su nombre");
+console.log("El nombre del usuario es: ",nombre_usuario);*/
+
+//Operador de Asignación
+/*let num1 = 5;
+let num2 = 10;
+
+console.log("Contenido de la varible num1: ",num1);
+console.log("Contenido de la variable num2: ",num2);
+
+num1 = num2
+console.log("La variable num1 contiene el mismo valor que la variable num2: ",num1);
+
+
+console.log("Contenido de la varible num1: ",num1);
+console.log("Contenido de la variable num2: ",num2);*/
+
+
+//Asignación de adición
+/*let num1 = 5;
+let num2 = 10;
+let resultado;
+
+console.log(num1)
+
+//Versión abreviada
+num1 += num2 //15
+resultado = num1 += num2;
+
+console.log(num1)*/
+
+//Asignación se sustración
+/*let num1 = 5;
+let num2 = 10;
+
+num2 -= num1;
+
+console.log(num2);*/
+
+
+//Asignación de multiplicación
+/*let num1 = 5;
+let num2 = 10;
+
+num2 *= num1;
+
+console.log(num2);*/
+
+
+//Asignación de división
+/*let num1 = 5;
+let num2 = 10;
+
+num2 /= num1;
+
+console.log(num2);*/
+
+
+//Asignación de resto o módulo
+/*let num1 = 2;
+let num2 = 25;
+let resultado;
+
+resultado = num2/num1;
+console.log(resultado);
+resultado = num2 % num1;
+console.log(resultado);*/
+
+//Asignación de exponenciación
+/*let num1 = 6;
+let num2 = 2;
+
+num2 **= num1;
+console.log(num2);*/
+
+
+
+
+
+
+
+
+
+
+
